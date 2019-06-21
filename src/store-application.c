fcd2fbc771aa15d5ac7932d2c0e5b8e234e86d5e
Robert Ancell <robert.ancell@canonical.com>
2019-06-21 13:25:13 +1200

    Use CSS for labels

--- src/store-application.c
+++ src/store-application.c
@@ -15,9 +15,24 @@ struct _StoreApplication
     GtkApplication parent_instance;
 
     StoreWindow *window;
+
+    GtkCssProvider *css_provider;
 };
 
 G_DEFINE_TYPE (StoreApplication, store_application, GTK_TYPE_APPLICATION)
+
+static void
+store_application_dispose (GObject *object)
+{
+    StoreApplication *self = STORE_APPLICATION (object);
+    g_clear_object (&self->css_provider);
+}
+
+static void
+theme_changed_cb (StoreApplication *self)
+{
+    gtk_css_provider_load_from_resource (self->css_provider, "/com/ubuntu/SnapStore/gtk-style.css");
+}
 
 static void
 store_application_startup (GApplication *application)
@@ -27,6 +42,11 @@ store_application_startup (GApplication *application)
     G_APPLICATION_CLASS (store_application_parent_class)->startup (application);
 
     self->window = store_window_new (self);
+
+    self->css_provider = gtk_css_provider_new ();
+    gtk_style_context_add_provider_for_screen (gdk_screen_get_default (), GTK_STYLE_PROVIDER (self->css_provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    g_signal_connect_object (gtk_settings_get_default (), "notify::gtk-theme-name", G_CALLBACK (theme_changed_cb), self, G_CONNECT_SWAPPED);
+    theme_changed_cb (self);
 }
 
 static void
@@ -39,6 +59,7 @@ store_application_activate (GApplication *application)
 static void
 store_application_class_init (StoreApplicationClass *klass)
 {
+    G_OBJECT_CLASS (klass)->dispose = store_application_dispose;
     G_APPLICATION_CLASS (klass)->startup = store_application_startup;
     G_APPLICATION_CLASS (klass)->activate = store_application_activate;
 }
